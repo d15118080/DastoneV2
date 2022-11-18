4d2b417bd1a2e89dd2f276edf69e6f63f505f3fa
d15118080 <66905665+d15118080@users.noreply.github.com>
2022-11-19 01:03:42 +0900

    1
    1

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Telegarm_set;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Telegram\Bot\Api;
 
@@ -16,13 +18,36 @@ class TelegramController extends Controller
         $username = $updates['message']['chat']['username'];
         $userid = $updates['message']['chat']['id'];
         $text = $updates['message']['text'];
-        //$api_key = explode(' ', $text);
         if (substr($text, 0, 4) == "/set") {
-            $a = substr($text,5) ;
-            $telegram->sendMessage([
-                'chat_id' => $userid,
-                'text' => "[테스트 가맹점] $username($userid)  l$a 님 등록되었습니다",
-            ]);
+            $key = substr($text, 5);
+
+            if (Telegarm_set::where('ck_id', $key)->exists()) {
+                $telegram->sendMessage([
+                    'chat_id' => $userid,
+                    'text' => "이미 등록되어 있습니다 관리자에게 문의 하세요.",
+                ]);
+
+            } else {
+
+                if (User::where('ck_id', $key)->exists()) {
+                    Telegarm_set::insert([
+                        'ck_id' => $key,
+                        'chat_id' => $userid,
+                    ]);
+                    $user_name = User::where('ck_id', $key)->value('user_name');
+                    $telegram->sendMessage([
+                        'chat_id' => $userid,
+                        'text' => "[$user_name] 님 알림 등록되었습니다",
+                    ]);
+                } else {
+                    $telegram->sendMessage([
+                        'chat_id' => $userid,
+                        'text' => "등록되지 않은 유저 입니다.",
+                    ]);
+
+                }
+
+            }
 
         }
     }
---
+++ app/Models/Telegarm_set.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Telegarm_set extends Model
+{
+    use HasFactory;
+
+        protected $fillable = [
+        'id',
+        'ck_id',
+        'chat_id'
+        ];
+}
